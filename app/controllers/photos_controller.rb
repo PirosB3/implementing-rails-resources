1cc1240fe030c9f059e737579c107ff6d4ac9de5
Daniel Pyrathon <pirosb3@gmail.com>
2013-04-28 23:46:07 +0100

    added new controller and view

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,9 @@ class PhotosController < ApplicationController
       @user = User.find(params[:user_id])
       @photos = @user.photos.all
   end
+
+  def new
+      @user = User.find(params[:user_id])
+      @photo = @user.photos.new
+  end
 end
