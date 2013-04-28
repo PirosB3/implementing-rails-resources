0beca32d6c7ca92eec7b41cbc19934da7b3d03e3
Daniel Pyrathon <pirosb3@gmail.com>
2013-04-28 23:55:54 +0100

    added validation and create method. view now displays errors too

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,16 @@ class PhotosController < ApplicationController
       @user = User.find(params[:user_id])
       @photo = @user.photos.new
   end
+
+  def create
+      @user = User.find(params[:user_id])
+      @photo = @user.photos.new params[:photo]
+
+      @photo.save
+      if @photo.valid?
+        redirect_to [@user, @photo]
+      else
+        render :new
+      end
+  end
 end
